21f40bfced6a591d005877b268546de2adf0473d
merrymustard <merryymustard@gmail.com>
2019-12-19 10:37:41 -0600

    .

--- server/app.js
+++ server/app.js
@@ -40,7 +40,7 @@ const app = express()
 
 app.use(function(req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:8888")
+  res.setHeader("Access-Control-Allow-Origin", process.env.FRONTENDPOINT)
 
   // Request methods you wish to allow
   res.setHeader(
