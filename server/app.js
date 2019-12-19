2059a0f978e1b219dd2d0f7683a6a8ef7344002f
merrymustard <merryymustard@gmail.com>
2019-12-19 10:35:25 -0600

    m

--- server/app.js
+++ server/app.js
@@ -31,19 +31,31 @@ const app = express()
 //   extended: true
 // }));
 
-app.use(
-  cors({
-    credentials: true,
-    origin: [process.env.FRONTENDPOINT]
-  })
-)
+// app.use(
+//   cors({
+//     credentials: true,
+//     origin: [process.env.FRONTENDPOINT]
+//   })
+// )
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", process.env.FRONTENDPOINT)
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:8888")
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
   )
+
+  // Request headers you wish to allow
+  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type")
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true)
+
+  // Pass to next layer of middleware
   next()
 })
 
